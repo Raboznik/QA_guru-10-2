cea6815a95c8bc79af879d38932cbf9562c92ba8
Maxim Kruchkov <mkruchkov@ayist.games>
2022-01-27 16:51:06 +0300

    add steps test

--- src/test/java/ru/yandex/mkruchkov/pages/TestBase.java
+++ src/test/java/ru/yandex/mkruchkov/pages/TestBase.java
@@ -1,40 +1,24 @@
 package ru.yandex.mkruchkov.pages;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import ru.yandex.mkruchkov.pages.helper.Attach;
-
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.util.Map;
 
 public class TestBase {
 
     @BeforeAll
-    static void setUp() throws MalformedURLException {
-
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setCapability("browserName", "chrome");
-//        capabilities.setCapability("browserVersion", "91.0");
-//        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-//                "enableVNC", true,
-//                "enableVideo", true
-//        ));
-//        RemoteWebDriver driver = new RemoteWebDriver(
-//                URI.create("https://user1:1234@selenoid.autotests.cloud/wd/hub").toURL(),
-//                capabilities
-//        );
+    static void setUp()  {
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
 
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
@@ -48,6 +32,7 @@ public class TestBase {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
+//        Selenide.closeWebDriver();
         Attach.addVideo();
     }
 }
--- src/test/java/ru/yandex/mkruchkov/pages/TestStudentRegistrationForm.java
+++ src/test/java/ru/yandex/mkruchkov/pages/TestStudentRegistrationForm.java
@@ -1,6 +1,10 @@
 package ru.yandex.mkruchkov.pages;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
 
 
 public class TestStudentRegistrationForm extends TestBase {
@@ -9,8 +13,61 @@ public class TestStudentRegistrationForm extends TestBase {
     RegistrationPage registrationPage = new RegistrationPage();
     RegistrationPageExam registrationPageExam = new RegistrationPageExam();
 
+    @DisplayName("Test with steps")
     @Test
+    void studentsRegistrationFormTest() {
+
+        step("Open main page", () -> {
+            registrationPage.openPage();
+        });
+        step("type first name", () -> {
+            registrationPage.typeFirstName(data.getName());
+        });
+        step("type last name", () -> {
+            registrationPage.typeLastName(data.getLastName());
+        });
+        step("type email", () -> {
+            registrationPage.typeEmail(data.getEmail());
+        });
+        step("select gender", () -> {
+            registrationPage.typeGender("Other");
+        });
+        step("type phone number", () -> {
+            registrationPage.typeNumber(data.getPhoneNumber());
+        });
+        step("select date birth", () -> {
+            registrationPage.typeBirthDate("28", "October", "1987");
+        });
+        step("type subjects", () -> {
+            registrationPage.typeSubjects("En").typeSubjects("P");
+        });
+        step("type hobbies", () -> {
+            registrationPage.typeHobbies("Reading");
+            registrationPage.typeHobbies("Music");
+        });
+        step("type current address", () -> {
+            registrationPage.typeCurrentAddress(data.getAddress());
+        });
+        step("select state", () -> {
+            registrationPage.selectState("Uttar");
+            registrationPage.selectCity("Agra");
+        });
+        step("submit form", () -> {
+            registrationPage.submit();
+        });
+
+        step("checking filled form ", () -> {
+            registrationPageExam.checkResultTable(data);
+        });
+        step("closing form", () -> {
+            registrationPageExam.closeResultTable();
+        });
+    }
+    @DisplayName("Test with dsl")
+    @Test
+    @Disabled
     void fillFormFieldsWithDsl() {
+
         registrationPage
                 .openPage()
                 .typeFirstName(data.getName())
@@ -34,3 +91,4 @@ public class TestStudentRegistrationForm extends TestBase {
 
     }
 }
+
